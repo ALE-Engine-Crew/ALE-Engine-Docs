45cf7fedd764823bfe7c1e7d2bf5702094828b9e
IamBritex <del.alc.jav@gmail.com>
2025-04-21 00:30:32 -0600

    feat: new "DUERMETE" animation for void, added "speakers" sprite, improved search system, minor bug fixes

--- source/voidPhaser.js
+++ source/voidPhaser.js
@@ -1,17 +1,36 @@
 // Configuración básica de Phaser
 const voidConfig = {
+    autofocus: false,
     type: Phaser.AUTO,
     parent: 'void-container',
     transparent: true,
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: '200%',
+        height: '200%',
     },
     scene: {
         preload: preloadVoid,
-        create: createVoid
+        create: createVoid,
     }
 };
+
+const speakerConfig = {
+    type: Phaser.AUTO,
+    parent: 'speaker-container',
+    transparent: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: '200%',
+        height: '200%',
+    },
+    scene: {
+        preload: preloadSpeaker,
+        create: createSpeaker,
+    }
+}
 
 // Variables globales
 let void_sprite, speaker_sprite;
@@ -24,31 +43,27 @@ const FADE_DURATION = 1000;
 const MAX_VOLUME = 0.7;
 
 // Ajustes visuales personalizados como quieras :v
-const VOID_SCALE = 3.5;
-const VOID_OFFSET_X = 150;
-const VOID_OFFSET_Y = 50;
-const SPEAKER_SCALE = 2.5;
-const SPEAKER_OFFSET_X = -200;
-const SPEAKER_OFFSET_Y = -50;
+const VOID_SCALE = 0.6;
+const VOID_OFFSET_X = 70;
+const VOID_OFFSET_Y = -150;
+const SPEAKER_SCALE = 0.4;
+const SPEAKER_OFFSET_X = 0;
+const SPEAKER_OFFSET_Y = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     voidGame = new Phaser.Game(voidConfig);
+    speakerGame = new Phaser.Game(speakerConfig);
 });
 
 function preloadVoid() {
     // PRECARGAMOS LOS RECURSOS DE MIERDAAAAAA GAAAAAAAAAAAA
     this.load.path = 'assets/images/Void/';
     this.load.atlasXML('void', 'Void.png', 'Void.xml');
-    this.load.atlasXML('speaker', 'bocina.png', 'bocina.xml');
-
-    this.load.path = 'assets/music/';
-    this.load.audio('danceMusic', 'girlfriendsRingtone.ogg');
 }
 
 function createVoid() {
     const scene = this;
 
-    // Creamos las putas animaciones de mierda de la puta void
     this.anims.create({
         key: 'void_dormida',
         frames: this.anims.generateFrameNames('void', {
@@ -85,7 +100,97 @@ function createVoid() {
         repeat: -1
     });
 
-    // Aca creas la unica puta animacion del puto parlante a la que le importa a nadie
+
+    this.anims.create({
+        key: 'void_DUERMETE',
+        frames: this.anims.generateFrameNames('void', {
+            prefix: 'DUERMETE',
+            start: 0,
+            end: 19,
+            zeroPad: 4
+        }),
+        frameRate: 24,
+        repeat: 0
+    });
+
+    void_sprite = this.add.sprite(0, 0, 'void', 'VoidDormida0000');
+    void_sprite.setOrigin(0.5, 1);
+    void_sprite.setScale(VOID_SCALE);
+    void_sprite.setInteractive();
+    void_sprite.setDepth(2);
+
+    positionVoidSprite.call(this);
+
+    void_sprite.on('pointerdown', () => {
+        if (currentAnimation === 'void_play' || currentAnimation === 'void_DUERMETE') return;
+    
+        previousState = currentAnimation;
+    
+        // Si está en idleDance y se hace clic, ejecutar DUERMETE
+        if (currentAnimation === 'void_idleDance') {
+            currentAnimation = 'void_DUERMETE';
+            void_sprite.play('void_DUERMETE');
+            fadeOutMusic();
+            stopSpeaker();
+            return;
+        }
+    
+        // Si no está en idleDance, hacer la animación de play como siempre
+        currentAnimation = 'void_play';
+        void_sprite.play('void_play');
+    
+        if (previousState === 'void_idleDance') {
+            fadeOutMusic();
+            stopSpeaker();
+        }
+    });
+    
+
+    void_sprite.on('animationstart', (animation) => {
+        if (animation.key === 'void_idleDance') {
+            fadeInMusic();
+            if (speaker_sprite) speaker_sprite.play('speaker_idle');
+        }
+    });
+
+    void_sprite.on('animationcomplete', (animation) => {
+        if (animation.key === 'void_play') {
+            if (previousState === 'void_dormida') {
+                currentAnimation = 'void_idleDance';
+                void_sprite.play('void_idleDance');
+            } else {
+                currentAnimation = 'void_dormida';
+                void_sprite.play('void_dormida');
+                fadeOutMusic();
+                stopSpeaker();
+            }
+        }
+    
+        // NUEVO: si termina DUERMETE, volver a dormida
+        if (animation.key === 'void_DUERMETE') {
+            currentAnimation = 'void_dormida';
+            void_sprite.play('void_dormida');
+        }
+    });
+    
+
+    void_sprite.play('void_dormida');
+
+    this.scale.on('resize', () => positionVoidSprite.call(scene));
+}
+function preloadSpeaker() {
+    this.load.path = 'assets/images/Void/';
+    this.load.atlasXML('speaker', 'bocina.png', 'bocina.xml');
+    this.load.path = 'assets/music/';
+    this.load.audio('danceMusic', 'chartEditorLoop.ogg');
+}
+
+function createSpeaker() {
+    speaker_sprite = this.add.sprite(0, 0, 'speaker', 'musicPlayer0009');
+    speaker_sprite.setOrigin(0.5, 1);
+    speaker_sprite.setScale(SPEAKER_SCALE);
+    speaker_sprite.setDepth(1);
+
     this.anims.create({
         key: 'speaker_idle',
         frames: this.anims.generateFrameNames('speaker', {
@@ -103,86 +208,35 @@ function createVoid() {
         loop: true
     });
 
-    spriteContainer = this.add.container(0, 0);
-    speakerContainer = this.add.container(0, 0);
-
-    // Creamos el contenedor de la void y el parlante (MATENME)
-    void_sprite = this.add.sprite(0, 0, 'void', 'VoidDormida0000');
-    void_sprite.setOrigin(0.5, 1);
-    void_sprite.setScale(VOID_SCALE);
-    void_sprite.setInteractive();
-    spriteContainer.add(void_sprite);
-
-    speaker_sprite = this.add.sprite(0, 0, 'speaker', 'musicPlayer0009');
-    speaker_sprite.setOrigin(0.5, 1);
-    speaker_sprite.setScale(SPEAKER_SCALE);
-    speakerContainer.add(speaker_sprite);
-
-    positionSprites.call(this);
-
-    // Agregamos el evento de puntero para la void
-    void_sprite.on('pointerdown', () => {
-        if (currentAnimation === 'void_play') return;
-
-        previousState = currentAnimation;
-        currentAnimation = 'void_play';
-        void_sprite.play('void_play');
-
-        if (previousState === 'void_idleDance') {
-            fadeOutMusic.call(scene);
-            stopSpeaker();
-        }
-    });
-
-    void_sprite.on('animationstart', (animation) => {
-        if (animation.key === 'void_idleDance') {
-            fadeInMusic.call(scene);
-            speaker_sprite.play('speaker_idle');
-        }
-    });
-
-    void_sprite.on('animationcomplete', (animation) => {
-        if (animation.key === 'void_play') {
-            if (previousState === 'void_dormida') {
-                currentAnimation = 'void_idleDance';
-                void_sprite.play('void_idleDance');
-            } else {
-                currentAnimation = 'void_dormida';
-                void_sprite.play('void_dormida');
-                fadeOutMusic.call(scene);
-                stopSpeaker();
-            }
-        }
-    });
-
-    void_sprite.play('void_dormida');
-
-    this.scale.on('resize', () => positionSprites.call(scene));
+    positionSpeakersSprite.call(this);
+    this.scale.on('resize', () => positionSpeakersSprite.call(this));
 }
 
-function positionSprites() {
-
+function positionVoidSprite() {
     const container = document.getElementById('void-container');
     if (!container) return;
 
-    // Obtenemos el tamaño del contenedor
-    const containerWidth = container.clientWidth;
-    const containerHeight = container.clientHeight;
+    const baseX = container.clientWidth / 2 + VOID_OFFSET_X;
+    const baseY = container.clientHeight - VOID_OFFSET_Y;
 
-    const baseX = containerWidth / 2;
-    const baseY = containerHeight - VOID_OFFSET_Y;
-
-    gsap.to(spriteContainer, {
-        x: baseX + VOID_OFFSET_X,
+    gsap.to(void_sprite, {
+        x: baseX,
         y: baseY,
         duration: 0.5,
         ease: 'power2.out'
     });
+}
 
-    // Posicionamos el parlante
-    gsap.to(speakerContainer, {
-        x: baseX + VOID_OFFSET_X + SPEAKER_OFFSET_X,
-        y: baseY + SPEAKER_OFFSET_Y,
+function positionSpeakersSprite() {
+    const container = document.getElementById('speaker-container');
+    if (!container) return;
+
+    const baseX = container.clientWidth / 2 + SPEAKER_OFFSET_X;
+    const baseY = container.clientHeight - SPEAKER_OFFSET_Y;
+
+    gsap.to(speaker_sprite, {
+        x: baseX,
+        y: baseY,
         duration: 0.5,
         ease: 'power2.out'
     });
@@ -192,7 +246,7 @@ function fadeInMusic() {
     if (!danceMusic || danceMusic.destroyed || danceMusic.isPlaying) return;
 
     // Aumentamos el volumen de la música
-    this.tweens.add({
+    speakerGame.scene.scenes[0].tweens.add({
         targets: danceMusic,
         volume: MAX_VOLUME,
         duration: FADE_DURATION,
@@ -206,8 +260,7 @@ function fadeInMusic() {
 function fadeOutMusic() {
     if (!danceMusic || danceMusic.destroyed || !danceMusic.isPlaying) return;
 
-    // Disminuimos el volumen de la música
-    this.tweens.add({
+    speakerGame.scene.scenes[0].tweens.add({
         targets: danceMusic,
         volume: 0,
         duration: FADE_DURATION,
@@ -219,19 +272,12 @@ function fadeOutMusic() {
 }
 
 function stopSpeaker() {
-    // Detenemos la animación del parlante
+    if (!speaker_sprite) return;
     speaker_sprite.anims.stop();
     speaker_sprite.setFrame('musicPlayer0009');
 }
 
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        if (voidGame?.scene?.scenes[0]) {
-            positionSprites.call(voidGame.scene.scenes[0]);
-        }
-    }, 500);
-});
-
-
-// salimy si ves esto, matame antes de que sea demasiado tarde
-// no me dejes vivir con esta verguenza, por favor
+function positionSprites() {
+    positionVoidSprite.call(this);
+    positionSpeakersSprite.call(this);
+}
